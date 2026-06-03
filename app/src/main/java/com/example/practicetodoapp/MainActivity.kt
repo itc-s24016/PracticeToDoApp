7158d2046f1dc7a1511baace6f866a032e40157a
itc-s24016 <s24016@std.it-college.ac.jp>
2026-06-03 14:47:46 +0900

    画面の骨組みの作成まで

--- app/src/main/java/com/example/practicetodoapp/MainActivity.kt
+++ app/src/main/java/com/example/practicetodoapp/MainActivity.kt
@@ -13,35 +13,97 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practicetodoapp.ui.theme.PracticeToDoAppTheme
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.sp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PracticeToDoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TodoTopBar(
+                showCompleted = false,
+                onToggleShowCompleted = { },
+                onDeleteCompleted = { },
+            )
+        },
+        floatingActionButton = {
+            TodoActionButton {
+                // 後で処理を書く
+            }
+        }
+        ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun TodoActionButton(onNewTodo: () -> Unit) {
+    FloatingActionButton(onClick = onNewTodo) {
+        Icon(Icons.Filled.Add, "add todo")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoTopBar (
+    modifier: Modifier = Modifier,
+    showCompleted: Boolean,
+    onToggleShowCompleted: (Boolean) -> Unit,
+    onDeleteCompleted: () -> Unit,
+){
+    TopAppBar(
+        modifier = modifier,
+        title = {Text("ToDo")},
+        actions = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("完了を表示", fontSize = 14.sp)
+                Switch(
+                    checked = showCompleted,
+                    onCheckedChange = onToggleShowCompleted
+                )
+                IconButton(
+                    onClick = onDeleteCompleted
+                ) {
+                    Icon(Icons.Filled.Delete, contentDescription = "完了したToDoを削除")
+                }
+            }
+        }
     )
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     PracticeToDoAppTheme {
-        Greeting("Android")
+        Main()
     }
 }
